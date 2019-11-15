ad9fea936f75591d7f26c964e0bc40d8d784b0e4
zspan93 <zg1993@gmail.com>
2019-11-15 16:16:22 -0500

    fix

--- Instagram/app/src/main/java/com/example/insta_green/LoginActivity.java
+++ Instagram/app/src/main/java/com/example/insta_green/LoginActivity.java
@@ -1,14 +1,68 @@
 package com.example.insta_green;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Login button clicked");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username, password);
+            }
+        });
+
+    }
+
+    private void login(String username, String password) {
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    //TODO better error handleing
+                    Log.e(TAG, "Issue with login");
+                    e.printStackTrace();
+                    return;
+                }
+            //TODO navigate to new acctiity on succeful on sign in
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Log.d(TAG, "Navigate to Main Activity");
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- Instagram/app/src/main/java/com/example/insta_green/ParseApplication.java
+++ Instagram/app/src/main/java/com/example/insta_green/ParseApplication.java
@@ -2,5 +2,23 @@ package com.example.insta_green;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("insta-green") // should correspond to APP_ID env variable
+                .clientKey("chatzs5")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("http://insta-green.herokuapp.com/parse").build());
+
+    }
 }
